caafced0b7c7bc8031e22ec264096b23b67dcef0
thatside <harammin@gmail.com>
2016-08-27 22:35:19 +0300

    Minor version change

--- vk-chat-shortcuts.user.js
+++ vk-chat-shortcuts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         vk-chat-shortcuts
 // @namespace    https://github.com/thatside/vk-chat-shortcuts/
-// @version      0.3.2
+// @version      0.3.3
 // @description  Allows to change active chats with Ctrl+(1-5) (only 5 last of them)
 // @author       thatside
 // @include      *vk.com*
